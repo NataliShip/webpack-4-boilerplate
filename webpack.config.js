8cbdb306db45522b82a996cadb1ae9b831c74360
Nataliya Shipelova <natulechka_m.25@mail.ru>
2020-05-06 17:12:38 +0300

    feat (webpack) - add css loader

--- webpack.config.js
+++ webpack.config.js
@@ -17,5 +17,13 @@ module.exports = {
       template: './index.html'
     }),
     new CleanWebpackPlugin()
-  ]
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader']
+      }
+    ]
+  }
 }
