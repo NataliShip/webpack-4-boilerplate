0b32db950e896c621349e7a74a8ded2b853f6de8
Nataliya Shipelova <natulechka_m.25@mail.ru>
2020-05-06 17:57:46 +0300

    feat (webpack) - add file-loader

--- webpack.config.js
+++ webpack.config.js
@@ -4,25 +4,42 @@ const { CleanWebpackPlugin } = require('clean-webpack-plugin')
 
 module.exports = {
   mode: 'development',
+
   entry: {
     main: './index.js'
   },
+
   output: {
     filename: '[name].[contenthash].js',
     path: path.resolve(__dirname, 'dist')
   },
+
+  resolve: {
+    extensions: ['js', 'jsx', 'css', 'sass']
+  },
+
   context: path.resolve(__dirname, 'src'),
+
   plugins: [
     new HTMLWebpackPlugin({
       template: './index.html'
     }),
     new CleanWebpackPlugin()
   ],
+
   module: {
     rules: [
       {
         test: /\.css$/,
         use: ['style-loader', 'css-loader']
+      },
+      {
+        test: /\.(jpe?g|svg|png|gif)$/,
+        use: ['file-loader']
+      },
+      {
+        test: /\.(ttf|woff|woff2|eot|otf)$/,
+        use: ['file-loader']
       }
     ]
   }
